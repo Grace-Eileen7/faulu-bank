c08936e3afd0c5e341361e7c239006c2216e2e94
Grace-Eileen7 <grace.eileen.bass@gmail.com>
2024-05-02 16:56:36 +0300

    updated routing

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,22 +1,30 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  standalone: true,
-  imports: [],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.css'
+  styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent {
-  isMenuOpen = false;
-  isPopupOpen = false;
+  showNotification: boolean = false;
 
-  toggleMenu() {
-    this.isMenuOpen = !this.isMenuOpen;
+  constructor(private router: Router) {}
+
+  validateLoginForm() {
+    const username = (<HTMLInputElement>document.getElementById('username')).value;
+    const password = (<HTMLInputElement>document.getElementById('password')).value;
+
+    if (username.trim() === '' || password.trim() === '') {
+      // If any field is empty, show notification
+      this.showNotification = true;
+    } else {
+      // If form is valid, navigate to layout component
+      this.router.navigate(['/dashboard']);
+    }
   }
 
-  togglePopup() {
-    this.isPopupOpen = !this.isPopupOpen;
+  hideNotification() {
+    this.showNotification = false;
   }
 }
-
